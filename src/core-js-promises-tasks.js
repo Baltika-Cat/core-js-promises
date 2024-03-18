62698d74b7ae3926f169f4071ca1b26b21d46fd5
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-03-18 06:09:28 +0300

    feat: partly complete last task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -168,8 +168,19 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return new Promise((resolve) => {
+    const arr = [];
+    new Promise(() => {})
+      .then(() => {
+        for (let i = 0; i < promises.length; i += 1) {
+          promises[i].then((result) => arr.push(result));
+        }
+      })
+      .finally(() => {
+        resolve(arr.join(''));
+      });
+  });
 }
 
 module.exports = {
